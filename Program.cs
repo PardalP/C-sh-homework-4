cacd883ce3a604cdab64b64a1fb754a150cb95ef
Pardal <pardal2608@gmail.com>
2023-05-24 22:50:56 +0600

    Init

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿Console.Clear();
+
+// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+// int Summa(int b, int a){
+//     int res = 1;
+//     for(int i = 0; i < b; i++){
+//         res *= a;
+//     }
+//     return res;
+// }
+
+
+// System.Console.WriteLine("Введите число которое нужно возвести в степень: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+
+// System.Console.WriteLine("Введите степень: ");
+// int b = Convert.ToInt32(Console.ReadLine());
+
+// System.Console.WriteLine($"{a} in power of {b} = {Summa(b, a)}");
+
+// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+// int Sum(int number)
+//     {
+//         int sum = 0;
+//         while (number != 0)
+//         {
+//             int a = number % 10;
+//             sum += a;
+//             number /= 10;
+//         }
+//         return sum;
+//     }
+
+// System.Console.WriteLine("Введите число : ");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// System.Console.WriteLine(Sum(number));
+
+
